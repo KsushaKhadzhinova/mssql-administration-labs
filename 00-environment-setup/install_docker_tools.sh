30ef651fbbb9ec933ba27836b791f234ea089fad
KsushaKhadzhinova <73707328+KsushaKhadzhinova@users.noreply.github.com>
2026-04-16 21:37:08 +0200

    Add script to install Docker and SQL tools

---
+++ 00-environment-setup/install_docker_tools.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg lsb-release docker.io docker-compose
+sudo systemctl enable docker
+sudo systemctl start docker
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
+curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list
+sudo apt-get update
+sudo ACCEPT_EULA=Y apt-get install -y mssql-tools18 unixodbc-dev
+echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
