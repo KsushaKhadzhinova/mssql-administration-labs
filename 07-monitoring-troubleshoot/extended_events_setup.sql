1921886af50524a7862b7ccd9cea6a6d612d4649
KsushaKhadzhinova <73707328+KsushaKhadzhinova@users.noreply.github.com>
2026-04-17 22:05:24 +0200

    Update extended_events_setup.sql for long query tracking

--- 07-monitoring-troubleshoot/extended_events_setup.sql
+++ 07-monitoring-troubleshoot/extended_events_setup.sql
@@ -1,12 +1,10 @@
 USE [master];
 GO
 
--- 1. Удаление сессии, если она уже существует
 IF EXISTS (SELECT * FROM sys.server_event_sessions WHERE name = 'TrackLongQueries')
     DROP EVENT SESSION [TrackLongQueries] ON SERVER;
 GO
 
--- 2. Создание сессии для отслеживания запросов дольше 1 секунды (1000000 микросекунд)
 CREATE EVENT SESSION [TrackLongQueries] ON SERVER 
 ADD EVENT sqlserver.sql_batch_completed(
     ACTION(sqlserver.sql_text, sqlserver.database_name)
@@ -14,10 +12,8 @@ ADD EVENT sqlserver.sql_batch_completed(
 ADD TARGET package0.event_file(SET filename=N'/var/opt/mssql/log/long_queries.xel');
 GO
 
--- 3. Запуск сессии
 ALTER EVENT SESSION [TrackLongQueries] ON SERVER STATE = START;
 GO
 
--- Проверка статуса сессий
 SELECT name, is_running FROM sys.dm_xe_sessions;
 GO
