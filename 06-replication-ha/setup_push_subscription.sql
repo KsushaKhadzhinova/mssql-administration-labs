b173be3688c463440e5328a5045656cf19af5cab
KsushaKhadzhinova <73707328+KsushaKhadzhinova@users.noreply.github.com>
2026-04-17 22:03:20 +0200

    Fix formatting and add newline in SQL script

--- 06-replication-ha/setup_push_subscription.sql
+++ 06-replication-ha/setup_push_subscription.sql
@@ -1,7 +1,6 @@
 USE [ProjectDB];
 GO
 
--- 1. Добавление подписки на издателе (sql1)
 EXEC sp_addsubscription 
     @publication = N'Pub_ProjectDB_Data', 
     @subscriber = N'sql2', 
@@ -13,10 +12,9 @@ EXEC sp_addsubscription
     @subscriber_type = 0;
 GO
 
--- 2. Добавление агента рассылки
 EXEC sp_addpushsubscription_agent 
     @publication = N'Pub_ProjectDB_Data', 
     @subscriber = N'sql2', 
     @subscriber_db = N'ProjectDB_Replica', 
-    @subscriber_security_mode = 1; -- Использовать учетную запись агента
+    @subscriber_security_mode = 1;
 GO
