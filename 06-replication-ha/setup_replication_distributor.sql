9656bada397c856bec31079be71b9d8531384352
KsushaKhadzhinova <73707328+KsushaKhadzhinova@users.noreply.github.com>
2026-04-17 10:57:31 +0200

    Fix newline at end of setup_replication_distributor.sql
    Added missing newline at the end of the file.

--- 06-replication-ha/setup_replication_distributor.sql
+++ 06-replication-ha/setup_replication_distributor.sql
@@ -1,12 +1,10 @@
 USE master;
 GO
-
 IF NOT EXISTS (SELECT * FROM sys.servers WHERE is_distributor = 1)
 BEGIN
     EXEC sp_adddistributor @distributor = N'sql1', @password = N'YourStrongPassword123!';
 END
 GO
-
 IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = N'distribution')
 BEGIN
     EXEC sp_adddistributiondb 
@@ -15,7 +13,6 @@ BEGIN
         @log_folder = N'/var/opt/mssql/data';
 END
 GO
-
 IF NOT EXISTS (SELECT * FROM msdb.dbo.MSdistpublishers WHERE name = N'sql1')
 BEGIN
     EXEC sp_adddistpublisher 
