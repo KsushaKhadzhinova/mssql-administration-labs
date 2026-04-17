acd6071df78800d8b0bccda182b80ed2ef0798c7
KsushaKhadzhinova <73707328+KsushaKhadzhinova@users.noreply.github.com>
2026-04-17 11:30:22 +0200

    Enhance distributor setup with retention parameters
    Added parameters for minimum and maximum distribution retention times in the distribution database setup.

--- 06-replication-ha/setup_replication_distributor.sql
+++ 06-replication-ha/setup_replication_distributor.sql
@@ -1,18 +1,26 @@
 USE master;
 GO
+
 IF NOT EXISTS (SELECT * FROM sys.servers WHERE is_distributor = 1)
 BEGIN
-    EXEC sp_adddistributor @distributor = N'sql1', @password = N'YourStrongPassword123!';
+    EXEC sp_adddistributor 
+        @distributor = N'sql1', 
+        @password = N'YourStrongPassword123!';
 END
 GO
+
 IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = N'distribution')
 BEGIN
     EXEC sp_adddistributiondb 
         @database = N'distribution', 
         @data_folder = N'/var/opt/mssql/data', 
-        @log_folder = N'/var/opt/mssql/data';
+        @log_folder = N'/var/opt/mssql/data', 
+        @min_distretention = 0, 
+        @max_distretention = 72, 
+        @history_retention = 48;
 END
 GO
+
 IF NOT EXISTS (SELECT * FROM msdb.dbo.MSdistpublishers WHERE name = N'sql1')
 BEGIN
     EXEC sp_adddistpublisher 
