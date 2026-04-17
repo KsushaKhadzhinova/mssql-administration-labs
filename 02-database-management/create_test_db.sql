d30d2cc5a70021ba1dc8835e0a54ff97c7ecc815
KsushaKhadzhinova <73707328+KsushaKhadzhinova@users.noreply.github.com>
2026-04-17 21:51:29 +0200

    Create test database with custom filegroup and NDF

--- 02-database-management/create_test_db.sql
+++ 02-database-management/create_test_db.sql
@@ -1,4 +1,3 @@
--- Create database with specific primary file properties
 CREATE DATABASE [Test]
  ON PRIMARY 
 ( NAME = N'testdata_a', FILENAME = N'/var/opt/mssql/data/testdata_a.mdf' , SIZE = 4096KB , MAXSIZE = 10240KB , FILEGROWTH = 2048KB )
@@ -6,11 +5,9 @@ CREATE DATABASE [Test]
 ( NAME = N'Test_log', FILENAME = N'/var/opt/mssql/data/Test_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB );
 GO
 
--- Add a custom filegroup
 ALTER DATABASE [Test] ADD FILEGROUP [TestFileGroup];
 GO
 
--- Add an NDF file to the new filegroup
 ALTER DATABASE [Test] ADD FILE 
 ( NAME = N'testdata_b', FILENAME = N'/var/opt/mssql/data/testdata_b.ndf' , SIZE = 5120KB , MAXSIZE = UNLIMITED, FILEGROWTH = 1024KB ) 
 TO FILEGROUP [TestFileGroup];
