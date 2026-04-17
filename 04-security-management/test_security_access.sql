508242160d467e2884d1221c0609bdf775eb08c5
KsushaKhadzhinova <73707328+KsushaKhadzhinova@users.noreply.github.com>
2026-04-17 22:00:07 +0200

    Update test_security_access.sql for access testing

--- 04-security-management/test_security_access.sql
+++ 04-security-management/test_security_access.sql
@@ -1,15 +1,12 @@
 USE [ProjectDB];
 GO
 
--- Test Access for TestUser2 (Employee)
 EXECUTE AS USER = 'TestUser2';
 GO
 PRINT 'Testing access for TestUser2...';
 
--- Should succeed (General access to dbo)
 SELECT TOP 1 * FROM dbo.Products;
 
--- Should fail (DENY was applied)
 BEGIN TRY
     SELECT * FROM SecureSchema.SalaryData;
 END TRY
