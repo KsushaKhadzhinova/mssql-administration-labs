60f917b0e72682b4b16e987494a53d52d56442fe
KsushaKhadzhinova <73707328+KsushaKhadzhinova@users.noreply.github.com>
2026-04-17 10:33:40 +0200

    Adjust sleep duration after sql1 restart
    Increased the sleep duration after restarting sql1 to allow more time for SQL Server initialization.

--- 03-backup-restore/simulate_corruption.sh
+++ 03-backup-restore/simulate_corruption.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
-# Определяем корневую папку проекта (на один уровень выше текущей папки 03)
 PROJECT_ROOT="$(cd "$(dirname "$0")/.." && pwd)"
 TARGET_FILE="$PROJECT_ROOT/volumes/sql1/data/testdata_a.mdf"
-
-echo "Stopping sql1 to simulate hardware failure..."
 docker stop sql1
-
-echo "Setting permissions for corruption..."
-# Выдаем права, чтобы dd точно сработал
 sudo chmod 666 "$TARGET_FILE"
-
-echo "Corrupting the primary data file..."
 sudo dd if=/dev/zero of="$TARGET_FILE" bs=1 count=4096 conv=notrunc
-
-echo "Restarting sql1..."
 docker start sql1
-echo "Waiting 15s for SQL Server to initialize..."
-sleep 15
+sleep 20
