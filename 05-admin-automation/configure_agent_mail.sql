20f8ab3bec7286cd5f6da1dcd5d645f6cc410b67
KsushaKhadzhinova <73707328+KsushaKhadzhinova@users.noreply.github.com>
2026-04-17 10:38:45 +0200

    Modify email addresses in configure_agent_mail.sql
    Updated email addresses for the Database Mail configuration.

--- 05-admin-automation/configure_agent_mail.sql
+++ 05-admin-automation/configure_agent_mail.sql
@@ -1,32 +1,24 @@
--- 1. Enable Advanced Options and Database Mail
 EXEC sp_configure 'show advanced options', 1;
 RECONFIGURE;
 EXEC sp_configure 'Database Mail XPs', 1;
 RECONFIGURE;
 GO
 
--- 2. Create a Database Mail Profile
 EXECUTE msdb.dbo.sysmail_add_profile_sp
-    @profile_name = 'AdminProfile',
-    @description = 'Profile for administrative alerts';
+    @profile_name = 'AdminProfile';
 
--- 3. Create an account for the mail server (using a dummy SMTP for lab purposes)
 EXECUTE msdb.dbo.sysmail_add_account_sp
     @account_name = 'AdminAccount',
-    @description = 'Account for sending alerts',
     @email_address = 'kseniyakhadzhynava@gmail.com',
-    @display_name = 'SQL Server Automation',
-    @mailserver_name = 'localhost'; -- In production, use your SMTP server
+    @mailserver_name = 'localhost'; 
 
--- 4. Add the account to the profile
 EXECUTE msdb.dbo.sysmail_add_profileaccount_sp
     @profile_name = 'AdminProfile',
     @account_name = 'AdminAccount',
     @sequence_number = 1;
 
--- 5. Create an Operator to receive notifications
 EXEC msdb.dbo.sp_add_operator 
     @name = N'DBA_Admin', 
     @enabled = 1, 
-    @email_address = N'xju2005@gmail.com';
+    @email_address = N'kseniyakhadzhynava@gmail.com';
 GO
