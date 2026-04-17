b6f625e6b549e66ebb4aae7ec98ffeccbbb93d5e
KsushaKhadzhinova <73707328+KsushaKhadzhinova@users.noreply.github.com>
2026-04-17 21:56:31 +0200

    Update SQL script for database restoration on node 2

--- 03-backup-restore/restore_on_node2.sql
+++ 03-backup-restore/restore_on_node2.sql
@@ -1,7 +1,6 @@
 USE [master];
 GO
 
--- Restore on Instance 2 with file relocation
 RESTORE DATABASE [Test_Restored] 
 FROM DISK = N'/var/opt/mssql/backup/test_full.bak' 
 WITH MOVE 'testdata_a' TO '/var/opt/mssql/data/test_node2.mdf',
@@ -10,6 +9,5 @@ WITH MOVE 'testdata_a' TO '/var/opt/mssql/data/test_node2.mdf',
      REPLACE, RECOVERY;
 GO
 
--- Check the restored database
 SELECT name, state_desc FROM sys.databases WHERE name = 'Test_Restored';
 GO
