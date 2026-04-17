a54994a8b25c8299a25055b6b367f316199c9d75
KsushaKhadzhinova <73707328+KsushaKhadzhinova@users.noreply.github.com>
2026-04-17 10:57:10 +0200

    Refactor SQL commands for Database Mail configuration

--- 05-admin-automation/configure_agent_mail.sql
+++ 05-admin-automation/configure_agent_mail.sql
@@ -3,20 +3,15 @@ RECONFIGURE;
 EXEC sp_configure 'Database Mail XPs', 1;
 RECONFIGURE;
 GO
-
-EXECUTE msdb.dbo.sysmail_add_profile_sp
-    @profile_name = 'AdminProfile';
-
+EXECUTE msdb.dbo.sysmail_add_profile_sp @profile_name = 'AdminProfile';
 EXECUTE msdb.dbo.sysmail_add_account_sp
     @account_name = 'AdminAccount',
     @email_address = 'kseniyakhadzhynava@gmail.com',
     @mailserver_name = 'localhost'; 
-
 EXECUTE msdb.dbo.sysmail_add_profileaccount_sp
     @profile_name = 'AdminProfile',
     @account_name = 'AdminAccount',
     @sequence_number = 1;
-
 EXEC msdb.dbo.sp_add_operator 
     @name = N'DBA_Admin', 
     @enabled = 1, 
